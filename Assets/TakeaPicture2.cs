cf9033472a2a27158f4e36bb3189d359c150ef25
Maren Hall-Wieckert <mhallwie@Marens-MacBook-Pro.local>
2017-04-10 23:12:22 -0600

    Put in some code on setting the material for the sprite renderer, does not work yet.

---
+++ Assets/TakeaPicture2.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class TakeaPicture2 : MonoBehaviour 
+{
+	private string savePath = "Assets/emotion_detector/";
+	public int captureCounter = 0;
+	WebCamTexture webCamTexture;
+
+	IEnumerator Start() 
+	{
+		webCamTexture = new WebCamTexture(1280, 720, 60);
+		webCamTexture.Play();
+
+		// NOTE - you almost certainly have to do this here:
+
+		yield return new WaitForEndOfFrame(); 
+
+	}
+	public void Update()
+	{
+		if(captureCounter < 3){
+
+		
+			// it's a rare case where the Unity doco is pretty clear,
+			// http://docs.unity3d.com/ScriptReference/WaitForEndOfFrame.html
+			// be sure to scroll down to the SECOND long example on that doco page 
+
+			Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
+			photo.SetPixels(webCamTexture.GetPixels());
+			photo.Apply();
+
+			//Encode to a PNG
+			byte[] bytes = photo.EncodeToJPG();
+			//Write out the PNG. Of course you have to substitute your_path for something sensible
+			File.WriteAllBytes(savePath + captureCounter.ToString () + ".jpg", bytes);
+			++captureCounter;
+
+			print ("captureCounter = " + captureCounter);
+		}
+	}
+	public void retakePics()
+	{
+		captureCounter = 0;
+	}
+}
---
+++ Assets/addface.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class addface : MonoBehaviour {
+	int picTotal = 0;
+	public GameObject fartiface;
+	Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void saveNew(){
+		File.Copy ("Assets/emotion_detector/1.jpg", "Assets/savedPics/saved_" + picTotal+".jpg", true);
+		Texture2D tex = (Texture2D)Resources.Load ("Assets/savedPics/saved_" + picTotal+".jpg");
+		//Material mat = new Material (tex);
+		GameObject newObject = Instantiate (fartiface);
+		newObject.GetComponent<SpriteRenderer>().sharedMaterial.mainTexture = tex;
+
+
+		//newObject.GetComponent<SpriteRenderer>().material.mainTexture = tex;
+		++picTotal;
+		//sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+
+	}
+			}
+
